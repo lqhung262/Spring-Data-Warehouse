572179fc64e6145626ef9106ec9119c2aaa3a71b
Lqhung <lqhung262@gmail.com>
2025-11-03 10:38:27 +0700

    fourth commit

---
+++ src/main/java/com/example/demo/controller/systemuser/AuthenticationController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller.systemuser;
+
+import com.example.demo.dto.ApiResponse;
+import com.example.demo.dto.systemuser.Authentication.AuthenticationRequest;
+import com.example.demo.dto.systemuser.Authentication.AuthenticationResponse;
+import com.example.demo.dto.systemuser.Introspect.IntrospectRequest;
+import com.example.demo.dto.systemuser.Introspect.IntrospectResponse;
+import com.nimbusds.jose.JOSEException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationController {
+    AuthenticationService authenticationService;
+
+
+    @PostMapping("/token")
+    ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
+        var result = authenticationService.authenticate(authenticationRequest);
+
+        return ApiResponse.<AuthenticationResponse>builder()
+                .result(result)
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request)
+            throws ParseException, JOSEException {
+        var result = authenticationService.introspect(request);
+
+        return ApiResponse.<IntrospectResponse>builder()
+                .result(result)
+                .build();
+    }
+}
